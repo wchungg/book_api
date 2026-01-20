67c3c14da0523ee416aa0d2722ff7a62e4086a64
William Chung <williamchung@ucsb.edu>
2026-01-19 20:57:58 -0800

    add put request to update books

--- main.py
+++ main.py
@@ -19,4 +19,12 @@ def get_book_by_id(id: int, db: Session = Depends(get_db)):
 @app.post("/books/", response_model=schemas.Book)
 def create_new_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
     return services.create_book(db, book)
+
+@app.put("/books/{id}", response_model=schemas.Book)
+def update_book(book: schemas.BookCreate, id: int, db: Session = Depends(get_db)):
+    db_update = services.update_book(db, book, id)
+
+    if not db_update:
+        raise HTTPException(status_code=404, detail="Book Not Found")
     
+    return db_update
--- services.py
+++ services.py
@@ -15,3 +15,13 @@ def get_books(db: Session):
 def get_book(db: Session, book_id: int):
     return db.query(Book).filter(Book.id==book_id).first()
 
+def update_book(db: Session, book: BookCreate, book_id: int):
+    book_queryset = db.query(Book).filter(Book.id==book_id).first()
+
+    if book_queryset:
+        for key, value in book.model_dump().items():
+            setattr(book_queryset, key, value)
+        db.commit()
+        db.refresh(book_queryset)
+
+    return book_queryset

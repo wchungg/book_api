1feed7c2f69e08e199e771e10c55dc7e895cd869
William Chung <williamchung@ucsb.edu>
2026-01-19 20:33:55 -0800

    create services to create and get books

---
+++ services.py
@@ -0,0 +1,13 @@
+from models import Book
+from sqlalchemy.orm import Session
+from schemas import BookCreate
+
+def create_book(db: Session, data: BookCreate):
+    book_instance = Book(**data.model_dump())
+    db.add(book_instance)
+    db.commit()
+    db.refresh()
+    return book_instance
+
+def get_books(db: Session):
+    return db.query(Book).all()
